3029fec1b0d63d7e2ec56e50fc21f224c028c7cc
srnag4 <srivathsa.nagaraj@ingka.com>
2025-04-23 15:23:37 +0530

    Lazy loading of components

--- support-tool-react-client/src/App.tsx
+++ support-tool-react-client/src/App.tsx
@@ -1,56 +1,34 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./Components/home";
-import { Login } from "./Components/authentication/login";
 import { Header } from "./Components/common-components/header";
 import { Sidebar } from "./Components/common-components/sidebar";
 import { BreadcrumbNavigator } from "./Components/common-components/breadcrumbs";
-import { SupportUsers } from "./Components/support-users";
-import { Modules } from "./Components/modules";
 import { AppContextProvider } from "./Context/AppContext";
-import ProtectedRoute from "./Components/common-components/auth/ProtectedRoute";
-import { Contents } from "./Components/contents";
-import { Users } from "./Components/users";
-import { Forms } from "./Components/forms";
-import { ListSystemSettings } from "./Components/system-settings/list";
-import { Edit } from "./Components/system-settings/edit";
-import { SystemSettings } from "./Components/system-settings";
 import { Notification } from "./Components/common-components/notifications";
-import { Create } from "./Components/system-settings/create";
+
+const LazyApp = React.lazy(
+  () => import("./Components/common-components/Lazy/index")
+);
 
 function App() {
-
   return (
-    <BrowserRouter>
-    <AppContextProvider>
-      <div className="App">
-        <Header />
-        <div className="flex-container">
-          <Sidebar />
-          <div className="content-container">
-            <BreadcrumbNavigator />
-              <Routes>
-                <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-                <Route path="/support-users" element={<ProtectedRoute><SupportUsers /></ProtectedRoute>} />
-                <Route path="/modules" element={<ProtectedRoute><Modules /></ProtectedRoute>} />
-                <Route path="/contents" element={<ProtectedRoute><Contents /></ProtectedRoute>} />
-                <Route path="/users" element={<ProtectedRoute><Users /></ProtectedRoute>} />
-                <Route path="/forms" element={<ProtectedRoute><Forms /></ProtectedRoute>} />
-                <Route path="/system-settings" element={<ProtectedRoute><SystemSettings /></ProtectedRoute>} >
-                  <Route index element={<ListSystemSettings />} />
-                  <Route path="edit/:id" element={<Edit />} />
-                  <Route path="create" element={<Create />} />
-                </Route>
-              </Routes>
+      <AppContextProvider>
+        <div className="App">
+          <Header />
+          <div className="flex-container">
+            <Sidebar />
+            <div className="content-container">
+              <BreadcrumbNavigator />
+              <div>
+                <Suspense fallback={<div>Loading...</div>}>
+                  <LazyApp />
+                </Suspense>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <Notification />
+        <Notification />
       </AppContextProvider>
-    </BrowserRouter>
   );
 }
 
---
+++ support-tool-react-client/src/Components/common-components/Lazy/index.tsx
@@ -0,0 +1,140 @@
+import { Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import ProtectedRoute from "../auth/ProtectedRoute";
+
+const Home = lazy(() =>
+  import("../../home/index").then((module) => ({
+    default: module.Home,
+  }))
+);
+
+const Login = lazy(() =>
+  import("../../authentication/login/index").then((module) => ({
+    default: module.Login,
+  }))
+);
+const SupportUsers = lazy(() =>
+  import("../../support-users/index").then((module) => ({
+    default: module.SupportUsers,
+  }))
+);
+const Modules = lazy(() =>
+  import("../../modules/index").then((module) => ({
+    default: module.Modules,
+  }))
+);
+const Contents = lazy(() =>
+  import("../../contents/index").then((module) => ({
+    default: module.Contents,
+  }))
+);
+const Users = lazy(() =>
+  import("../../users/index").then((module) => ({
+    default: module.Users,
+  }))
+);
+const Forms = lazy(() =>
+  import("../../forms/index").then((module) => ({
+    default: module.Forms,
+  }))
+);
+const SystemSettings = lazy(() =>
+  import("../../system-settings/index").then((module) => ({
+    default: module.SystemSettings,
+  }))
+);
+const ListSystemSettings = lazy(() =>
+  import("../../system-settings/list").then((module) => ({
+    default: module.ListSystemSettings,
+  }))
+);
+const Edit = lazy(() =>
+  import("../../system-settings/edit").then((module) => ({
+    default: module.Edit,
+  }))
+);
+const Create = lazy(() =>
+  import("../../system-settings/create").then((module) => ({
+    default: module.Create,
+  }))
+);
+
+const LazyApp = () => {
+  return (
+      <Suspense fallback={<div>Loading route...</div>}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/support-users"
+            element={
+              <ProtectedRoute>
+                <SupportUsers />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/modules"
+            element={
+              <ProtectedRoute>
+                <Modules />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/contents"
+            element={
+              <ProtectedRoute>
+                <Contents />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/users"
+            element={
+              <ProtectedRoute>
+                <Users />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/forms"
+            element={
+              <ProtectedRoute>
+                <Forms />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/system-settings"
+            element={
+              <ProtectedRoute>
+                <SystemSettings />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<ListSystemSettings />} />
+            <Route path="edit/:id" element={<Edit />} />
+            <Route path="create" element={<Create />} />
+          </Route>
+        </Routes>
+      </Suspense>
+  );
+};
+
+export default LazyApp;
