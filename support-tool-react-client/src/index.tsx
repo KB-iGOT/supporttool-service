9fd4d23fb7342cae2c3dc1af2c01dcf2b6e0e8dc
Bharath R <bharathravi.in@gmail.com>
2026-02-18 22:03:53 +0530

    changes in env json

--- support-tool-react-client/src/index.tsx
+++ support-tool-react-client/src/index.tsx
@@ -5,8 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
-// Wait for the env-config.json fetch (started in index.html <head>) to complete,
-// then render the app so that window._env_ is fully populated before any component reads it.
+// Wait for env.json fetch (started in index.html <head>) to complete,
+// so window._env_ is fully populated before any component reads it.
 (window as any)._envConfigPromise
   ? (window as any)._envConfigPromise.finally(renderApp)
   : renderApp();
@@ -20,8 +20,5 @@ function renderApp() {
       <App />
     </BrowserRouter>
   );
-  // If you want to start measuring performance in your app, pass a function
-  // to log results (for example: reportWebVitals(console.log))
-  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
   reportWebVitals();
 }
