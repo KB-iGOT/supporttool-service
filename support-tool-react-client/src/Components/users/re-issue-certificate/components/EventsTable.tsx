2b4ce206ade964244a799137695cc940fe83cdf5
Bharath Ravi <117885618+bharathravi-in@users.noreply.github.com>
2026-04-10 13:46:50 +0530

    Cbrelease 4.8.30 (#21)
    * changes in the portal for user issue and org
    
    * changes in the user and org changes and copy options
    
    * enrollment api integration
    
    * changes for user details
    
    * changes in the playlist creation
    
    * changes for read mode
    
    * changes in the portal for user data to view
    
    * changes for the form service
    
    * changes in commenting master-language api call
    
    * changes in the re-issue certificatte
    
    * re-issue certificate is disabled

--- support-tool-react-client/src/Components/users/re-issue-certificate/components/EventsTable.tsx
+++ support-tool-react-client/src/Components/users/re-issue-certificate/components/EventsTable.tsx
@@ -89,7 +89,7 @@ const CollapsibleEventRow: React.FC<CollapsibleEventRowProps> = ({
           )}
         </TableCell>
         <TableCell>
-          {event.issuedCertificates && event.issuedCertificates.length > 0 ? (
+          {/* {event.issuedCertificates && event.issuedCertificates.length > 0 ? (
             <Box sx={{ display: 'flex', gap: 1 }}>
               <Button
                 variant="outlined"
@@ -120,7 +120,7 @@ const CollapsibleEventRow: React.FC<CollapsibleEventRowProps> = ({
             >
               Re-issue
             </Button>
-          )}
+          )} */}
         </TableCell>
       </TableRow>
       <TableRow>
